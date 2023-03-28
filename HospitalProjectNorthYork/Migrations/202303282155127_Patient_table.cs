9813efab46fe2b615e949a1c0b6c9da3e7f79d9f
aadri <aadritc@gmail.com>
2023-03-28 17:57:13 -0400

    patient table

---
+++ HospitalProjectNorthYork/Migrations/202303282155127_Patient_table.cs
@@ -0,0 +1,28 @@
+﻿namespace HospitalProjectNorthYork.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Patient_table : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Patients",
+                c => new
+                    {
+                        Patient_ID = c.Int(nullable: false, identity: true),
+                        PatientName = c.String(),
+                        PatientAdmittanceDate = c.DateTime(nullable: false),
+                        PatientDateOfBirth = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Patient_ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Patients");
+        }
+    }
+}
