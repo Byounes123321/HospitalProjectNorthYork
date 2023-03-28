aa1b8d96e024f5c0ce40c8417967254a133bc169
aadri <aadritc@gmail.com>
2023-03-28 17:29:44 -0400

    aadi tables

---
+++ HospitalProjectNorthYork/Models/FAQ.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HospitalProjectNorthYork.Models
+{
+    public class FAQ
+    {
+        [Key]
+        public int Faq_ID { get; set; }
+        //primary key
+        public string Ques { get; set; }
+        // questions
+        public string Answer { get; set; }
+        // answer
+    }
+}
---
+++ HospitalProjectNorthYork/Models/Visit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HospitalProjectNorthYork.Models
+{
+    public class Visit
+    {
+        [Key]
+        public int Visit_ID { get; set; }
+        // primary key 
+
+        public DateTime VisitDate { get; set; }
+        // date time of the appointment
+    }
+}
