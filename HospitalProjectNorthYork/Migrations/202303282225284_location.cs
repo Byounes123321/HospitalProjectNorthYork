add97ba438b0ee34b6860c3398af01a7acf55f33
Bassil Younes <bassilyounes@gmail.com>
2023-03-28 18:27:08 -0400

    Fixed errors

---
+++ HospitalProjectNorthYork/Migrations/202303282225284_location.cs
@@ -0,0 +1,27 @@
+﻿namespace HospitalProjectNorthYork.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class location : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Locations",
+                c => new
+                    {
+                        Location_ID = c.Int(nullable: false, identity: true),
+                        LocaitonName = c.String(),
+                        LocationDesc = c.String(),
+                    })
+                .PrimaryKey(t => t.Location_ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Locations");
+        }
+    }
+}
