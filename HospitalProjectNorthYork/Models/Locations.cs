aaef24c92776acc600c121de41a8232b940acd02
Bassil Younes <bassilyounes@gmail.com>
2023-03-28 17:48:31 -0400

    Created locations table

---
+++ HospitalProjectNorthYork/Models/Locations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace HospitalProjectNorthYork.Models
+{
+    public class Locations
+    {
+        [Key]
+        public int Location_ID { get; set; }
+        //Primary Key
+        public string LocaitonName { get; set; }
+        //Name of the location
+        public string LocationDesc { get; set; }
+        //Description of the location
+        
+    }
+}
